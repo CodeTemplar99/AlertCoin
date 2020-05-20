dbf240630fd4bc31659cb879b6415ce9799e09d8
Chiadika <udoagwac@gmail.com>
2020-05-20 20:50:00 +0100

    finished home with add link

--- js/spine.js
+++ js/spine.js
@@ -6,3 +6,19 @@ function showSideBar() {
     isOpen = !isOpen
 
 }
+
+
+// function increaseDaily() {
+
+//     // let dailyFigure = 3000;
+//     let seeDaily = 0;
+//     let i = 0;
+//     if (i < 3000) {
+//         seeDaily = i++;
+//         document.getElementById('daily-figure').innerHTML = seeDaily;
+
+//     }
+// }
+// increaseDaily()
+
+// for (var i = 0; i < countryInfo.length; i += 1) {
