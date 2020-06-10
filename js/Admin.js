ce5f93cb6e1f35beac9de3b7aedc3b7aa922d2c3
Chiadika <udoagwac@gmail.com>
2020-06-10 22:19:08 +0100

    tawk.to

--- js/Admin.js
+++ js/Admin.js
@@ -1,3 +1,36 @@
+function signUpNew() {
+    let email = document.querySelector("#inputEmail").value;
+    var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email !== "" && email.match(mailFormat)) {
+        console.log("match");
+    }
+    else {
+
+        console.log("email is empty");
+    }
+
+    let password = document.querySelector("#inputPassword").value;
+    if (password == "") {
+        console.log("password is empty");
+    }
+
+    let ref = FIRDatabase.database().reference().root;
+    if (email !== "" && password !== "") {
+        console.log("email and password not empty");
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+        });
+
+    } else {
+    }
+}
+document.querySelector("#signUpNow").addEventListener("click", () => {
+    signUpNew()
+});
+
 // for users
 document.querySelector("#users-tab").addEventListener("click", () => {
     $(".views").not("#table-con").hide();
@@ -18,10 +51,9 @@ document.querySelector("#mail_tab").addEventListener("click", () => {
     $("#web-mail-con").css("display", "block")
     function RealMailView() {
         // var superLevel = $("#web-mail-con")
-
         var l1 = document.createElement("iframe");
         var attributes = ["src", "title", "width", "height"];
-        var attValues = ["https://mdn-samples.mozilla.org/snippets/html/iframe-simple-contents.html", "iframe example 1", "100%", "100%"];
+        var attValues = ["https:www.google.com", "iframe", "100%", "100%"];
 
         for (var i = 0; i < attributes.length; i++) {
             l1.setAttribute(attributes[i], attValues[i])
